8e6f60a546e2686a9d604a8e7490adb823cc120b
ryoma-abe <liaozhenanbu387@gmail.com>
2025-07-05 06:56:11 +0900

    アカウントページにセクションヘッダーを追加し、ユーザーの役割に応じたコンテンツを表示するロジックを実装しました。また、ヘッダーコンポーネントを非同期関数に変更し、ユーザー情報の取得を追加しました。ダッシュボードページを削除し、ナビゲーション項目を更新しました。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Header from "@/components/common/header";
 import MainContent from "@/components/common/main-content";
+import Header from "@/components/common/header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,22 +1,32 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
 import NavItems from "./nav-items";
+import { createClient } from "@/utils/supabase/server";
 
-export default function Header() {
+export default async function HeaderGate() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
   return (
-    <header className="flex items-center px-6 py-4 bg-white shadow-sm sticky top-0 z-50">
-      <h1 className="text-2xl font-bold text-gray-900">
-        <Link href="/">StreamLearn</Link>
-      </h1>
-      <NavItems />
-      <div className="flex items-center space-x-2">
-        <Button variant="secondary">
-          <Link href="/login">ログイン</Link>
-        </Button>
-        <Button>
-          <Link href="/signup">新規登録</Link>
-        </Button>
-      </div>
-    </header>
+    <div>
+      <header className="flex items-center w-full justify-between px-6 py-4 bg-white shadow-sm sticky top-0 z-50">
+        <h1 className="text-2xl font-bold text-gray-900">
+          <Link href="/">StreamLearn</Link>
+        </h1>
+        {user ? (
+          <NavItems />
+        ) : (
+          <div className="flex items-center space-x-2">
+            <Button variant="secondary">
+              <Link href="/login">ログイン</Link>
+            </Button>
+            <Button>
+              <Link href="/signup">新規登録</Link>
+            </Button>
+          </div>
+        )}
+      </header>
+    </div>
   );
 }
