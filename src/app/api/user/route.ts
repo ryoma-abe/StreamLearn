ad83a08dd51836dc45f2491211d893b6854fe1e6
ryoma-abe <liaozhenanbu387@gmail.com>
2025-07-15 09:36:19 +0900

    受講教材取得APIを修正し、ユーザー情報の取得方法をコースから購入に変更しました。また、コメントを更新しました。

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -2,7 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
 
-// 教材取得
+// 受講教材取得
 export async function GET() {
   const supabase = await createClient();
   const {
@@ -14,14 +14,14 @@ export async function GET() {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
-  const myCourses = await prisma.course.findMany({
+  const myUserCourses = await prisma.purchase.findMany({
     where: {
-      instructor: {
+      user: {
         email: user?.email,
       },
     },
     orderBy: { createdAt: "desc" },
   });
 
-  return NextResponse.json(myCourses);
+  return NextResponse.json(myUserCourses);
 }
