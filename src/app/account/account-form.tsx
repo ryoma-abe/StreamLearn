6e452fbeccd3a088c20905d73ee30562c9f43201
ryoma-abe <liaozhenanbu387@gmail.com>
2025-07-15 09:27:02 +0900

    AccountFormコンポーネントを非同期関数に変更し、Supabaseから現在ログインしているユーザーの情報を取得して表示するようにしました。また、ユーザー名とメールアドレスの表示をデータベースから取得するように修正しました。

--- src/app/account/account-form.tsx
+++ src/app/account/account-form.tsx
@@ -1,5 +1,22 @@
-"use client";
-export default function AccountForm() {
+import { prisma } from "@/lib/prisma";
+import { createClient } from "@/utils/supabase/server";
+
+export default async function AccountForm() {
+  const supabase = await createClient();
+
+  // ここで現在ログインしているユーザーの情報を取得
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // Prismaとログインユーザーの紐付け
+  const dbUser = user
+    ? await prisma.user.findUnique({
+        where: { email: user.email! },
+      })
+    : null;
+  console.log(dbUser);
+
   return (
     <div className="rounded-lg border p-6">
       <h3 className="mb-4">アカウント情報</h3>
@@ -14,22 +31,22 @@ export default function AccountForm() {
           <input
             id="email"
             type="text"
-            value={"メールアドレス"}
+            value={dbUser?.email}
             disabled
             className="w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-50"
           />
         </div>
         <div className="space-y-2">
           <label
-            htmlFor="email"
+            htmlFor="name"
             className="block text-sm font-medium text-gray-700"
           >
-            Email
+            名前
           </label>
           <input
-            id="email"
+            id="name"
             type="text"
-            value={"メールアドレス"}
+            value={dbUser?.name ?? ""}
             disabled
             className="w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-50"
           />
